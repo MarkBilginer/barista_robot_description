f2b5930e1e8c7e8ec540cfe94e976312f6631c86
MarkBilginer <markbilginer@gmail.com>
2024-05-29 13:48:35 +0000

    Added improved laser_scan_link and joint. Laser now shows in both rviz and gazebo. Updated barista_urdf.launch.py to work correctly with gazebo. Fixed the bug where gazebo could not find the laser meshes. Added gazebo_ros, robot_state_publisher and joint_state_publisher to both package.xml and CMakeLists.txt. Created a new subfolder under /meshes to group all 2D lidars in /LIDAR_2D_Sensor. Added multiple 2D Lidar .dae files, meshes, into /LIDAR_2D_Sensor. Added README.md into /LIDAR_2D_Sensor, to show the source of the LIDAR meshes.

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -2,8 +2,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from ament_index_python.packages import get_package_prefix
 
 # this is the function launch  system will look for
 def generate_launch_description():
@@ -13,12 +16,53 @@ def generate_launch_description():
     #xacro_file = "box_bot.xacro"
     package_description = "barista_robot_description"
 
+    # Paths
+    pkg_barista_robot_description = get_package_share_directory(package_description)
+    install_dir = get_package_prefix(package_description)
+
+    print("Resolved package path for barista_robot_description: ", pkg_barista_robot_description)
+    
+    # Setting up Gazebo environment variables
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    gazebo_models_path = os.path.join(pkg_barista_robot_description, 'models')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    world_file_arg = DeclareLaunchArgument(
+        'world',
+        default_value=[get_package_share_directory('barista_robot_description'), 'gazebo/worlds/barista_robot_empty.world'],
+        description= "barista_robot_empty_world",
+    )
+    # Define the launch arguments for the Gazebo launch file
+    gazebo_launch_args = {
+        'verbose': 'false',
+        'pause': 'false',
+        'world': LaunchConfiguration('world')
+    }
+
+    # Include the Gazebo launch file with the modified launch arguments
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments=gazebo_launch_args.items(),
+    )
+
     ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
 
     # Robot State Publisher
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -40,10 +84,21 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True}],
             arguments=['-d', rviz_config_dir])
 
+    # Spawn Entity Node
+    spawn_entity_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'barista_robot', '-topic', 'robot_description'],
+        output='screen'
+    )
+
     # create and return launch description object
     return LaunchDescription(
         [            
+            world_file_arg,
+            gazebo,
             robot_state_publisher_node,
-            rviz_node
+            rviz_node,
+            spawn_entity_node
         ]
     )
---
+++ meshes/LIDAR_2D_Sensor/README.md
@@ -0,0 +1,10 @@
+# LIDAR 2D Sensor
+
+## Description
+This folder contains the 3D meshes/models for the LIDAR 2D sensor used in the project. These models are essential for simulating the LIDAR sensor accurately within the robot simulation environment.
+
+## Acknowledgment
+
+The 3D meshes/models for the LIDAR 2D sensor used in this project are sourced from the `checkpoint4_auxiliary_files` repository. We gratefully acknowledge the work done by Robotnik Automation, and the models can be found at the following URL:
+
+[Robotnik Automation - Robot Sensors](https://github.com/RobotnikAutomation/robotnik_sensors)
