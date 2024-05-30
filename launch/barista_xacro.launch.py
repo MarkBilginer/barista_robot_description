7d4530d25e16948eea1152b26ab093788f3e04cf
MarkBilginer <markbilginer@gmail.com>
2024-05-30 05:37:08 +0000

    Created main xacro file with robot_description: barista_robot_model.urdf.xacro; created xacro macros inside wheel.xacro, standoff.xacro, cup_holder_tray.xacro. Created launch file for barista_robot_model.xacro: barista_xacro.launch.py. Added /xacro directory inside CMakeLists.txt as install directory.

---
+++ launch/barista_xacro.launch.py
@@ -0,0 +1,105 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_prefix
+
+import xacro
+
+
+def generate_launch_description():
+
+        ####### DATA INPUT ##########
+    xacro_file = 'barista_robot_model.urdf.xacro'
+    package_description = "barista_robot_description"
+
+    # Paths
+    pkg_barista_robot_description = get_package_share_directory(package_description)
+    install_dir = get_package_prefix(package_description)
+
+    print("Resolved package path for barista_robot_description: ", pkg_barista_robot_description)
+
+     # Setting up Gazebo environment variables
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    gazebo_models_path = os.path.join(pkg_barista_robot_description, 'models')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    world_file_arg = DeclareLaunchArgument(
+        'world',
+        default_value=[get_package_share_directory('barista_robot_description'), 'gazebo/worlds/barista_robot_empty.world'],
+        description= "barista_robot_empty_world",
+    )
+    # Define the launch arguments for the Gazebo launch file
+    gazebo_launch_args = {
+        'verbose': 'false',
+        'pause': 'false',
+        'world': LaunchConfiguration('world')
+    }
+
+    # Include the Gazebo launch file with the modified launch arguments
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments=gazebo_launch_args.items(),
+    )
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(pkg_barista_robot_description, "xacro", xacro_file)
+
+    # convert XACRO file into URDF
+    doc = xacro.parse(open(robot_desc_path))
+    xacro.process_doc(doc)
+    params = {'use_sim_time': True, 'robot_description': doc.toxml()}
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='my_robot_state_publisher_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[params]
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
+    # Spawn Entity Node
+    spawn_entity_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'barista_robot', '-topic', 'robot_description'],
+        output='screen'
+    )
+
+
+    return LaunchDescription([
+        world_file_arg,
+        gazebo,
+        robot_state_publisher_node,
+        rviz_node,
+        spawn_entity_node,
+    ])
